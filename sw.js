fe4156abf0b76ff14507c4b3d01955336c1f6609
MANJUNATH Y <manjunath8495839565@gmail.com>
2026-09-21 23:50:06 +0530

    Updates

--- sw.js
+++ sw.js
@@ -53,3 +53,20 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+// Notification click event handler
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) {
+        if (client.url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('./');
+      }
+    })
+  );
+});
